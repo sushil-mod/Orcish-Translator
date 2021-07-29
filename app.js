3f2cf83c7482960f06f3daef15091e5c5aedb003
sushil-mod <78729065+sushil-mod@users.noreply.github.com>
2021-07-29 23:19:43 +0530

    docs: added js and html code

---
+++ app.js
@@ -0,0 +1,19 @@
+var textInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
+var btnTranslate =document.querySelector("#translate-button");
+
+var serverURL = "https://api.funtranslations.com/translate/pirate.json";
+
+btnTranslate.addEventListener("click",clickHandler)
+
+function clickHandler(){
+
+    fetch(getURl(textInput.value))
+    .then(response => response.json())
+    .then(json => {txtOutput.innerText = json.contents.translated})
+
+}
+
+function getURl(input){
+    return serverURL + "?" + "text=" + input;
+}
